2ce086490a6e7470c58855f951299d6f3b87c7ef
Ebube Okonji <ebube.okonji2@gmail.com>
2021-07-27 14:41:46 +0100

    Add all my files

---
+++ js/landingpage.js
@@ -0,0 +1,13 @@
+document.getElementById('popular').addEventListener('click', function() { document.getElementById('show').style.display = "block";})
+document.getElementById('popular1').addEventListener('click', function() {
+  document.getElementById('row3').style.display = "block";
+})
+
+//Handle Increment
+handleIncrement() {
+ // let increase = document.getElementById('quantity').value;
+  //let count = 1;
+ // count++;
+  alert('Button clicked!');
+  //increase.innerHTML = count;
+}
